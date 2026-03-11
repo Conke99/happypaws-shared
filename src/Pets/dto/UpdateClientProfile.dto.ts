2375f3ab1fe15ca63bd61838cd239b72b1538528
Aleksandar Conic <acaconic99@gmail.com>
2026-03-12 00:37:43 +0100

    Add UpdateClientProfileDto class to implement user profile fields with validation

---
+++ src/Pets/dto/UpdateClientProfile.dto.ts
@@ -0,0 +1,52 @@
+import { IsOptional, IsString } from 'class-validator';
+import { UpdateClientProfileRequest } from '../../User/models/UpdateClientProfileRequest';
+
+export class UpdateClientProfileDto implements UpdateClientProfileRequest {
+  @IsOptional()
+  @IsString()
+  firstName?: string;
+
+  @IsOptional()
+  @IsString()
+  lastName?: string;
+
+  @IsOptional()
+  @IsString()
+  phone?: string | null;
+
+  @IsOptional()
+  @IsString()
+  photoUrl?: string | null;
+
+  @IsOptional()
+  @IsString()
+  bio?: string | null;
+
+  @IsOptional()
+  @IsString()
+  city?: string | null;
+
+  @IsOptional()
+  @IsString()
+  state?: string | null;
+
+  @IsOptional()
+  @IsString()
+  zipCode?: string | null;
+
+  @IsOptional()
+  @IsString()
+  emergencyContactName?: string | null;
+
+  @IsOptional()
+  @IsString()
+  emergencyContactRelationship?: string | null;
+
+  @IsOptional()
+  @IsString()
+  emergencyContactPhone?: string | null;
+
+  constructor(partial?: Partial<UpdateClientProfileRequest>) {
+    Object.assign(this, partial);
+  }
+}
